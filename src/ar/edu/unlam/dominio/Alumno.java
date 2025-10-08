b063ff49c389f56f82fe679791a4fb13ad991385
Mercado006 <smercado999@alumno.unlam.edu.ar>
2025-10-08 07:26:16 -0300

    Se arregló la clase Alumno

--- src/ar/edu/unlam/dominio/Alumno.java
+++ src/ar/edu/unlam/dominio/Alumno.java
@@ -75,6 +75,7 @@ public class Alumno {
 		if(trabajosPracticos.contains(trabajoPractico) && trabajoPractico.puedeSerEntregado(fechaEntrega)) {
 			EntregaTrabajoPractico entregaTrabajoPractico = new EntregaTrabajoPractico(this, fechaEntrega, trabajoPractico.getCursoAlQuePertenece());
 			trabajoPractico.getCursoAlQuePertenece().getProfesor().recibirEntregaTrabajoPractico(entregaTrabajoPractico);
+			trabajosPracticos.remove(trabajoPractico);
 			return true;
 		};
 		return false;
